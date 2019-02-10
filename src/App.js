b1fbaa62db07113e95e97b07a81f9f4fb96786ed
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-10 20:41:47 +0100

    add Navbar cmpt with a nice template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+// import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
       </div>
     );
   }
