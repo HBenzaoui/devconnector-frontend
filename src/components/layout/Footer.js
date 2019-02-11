adec412387b9b016dcdc3934cbf31c5c36c9108b
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-11 18:15:58 +0100

    change title and footer some text

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Made with by @HBenzasoui
+      Made with 💻 by{' '}
+      <a href="https://github.com/HBenzaoui" target="_blank">
+        @HBenzasoui
+      </a>
     </footer>
   );
 };
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ class Navbar extends Component {
         >
           <div className="container">
             <a className="navbar-brand" href="landing.html">
-              DevConnector
+              Conneected;DEV
             </a>
             <button
               className="navbar-toggler"
