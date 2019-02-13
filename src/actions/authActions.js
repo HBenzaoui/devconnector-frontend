7c2caf80bad928077aa1228bff83cbb67c514627
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-13 19:45:12 +0100

    instal jwt-decode package

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,11 +1,33 @@
 import axios from 'axios';
 
 import { GET_ERRORS } from './type';
+import setAuthToken from '../utils/setAuthToken';
+
 //Register new user
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post('/api/users/register', userData)
     .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Login new user
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => {
+      //save to local storage
+      const { token } = res.data;
+      //set token to localstorage
+      localStorage.setItem('jwtToken', token);
+      //set token to auth header
+      setAuthToken(token);
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
