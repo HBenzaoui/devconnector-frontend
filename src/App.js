af7ca26de32a48574dd054332434772907be38da
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-13 21:09:53 +0100

    set the curent user in App.js with it token

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 
 import Navbar from './components/layout/Navbar';
@@ -10,6 +14,16 @@ import Login from './components/auth/Login';
 import store from './store';
 
 import './App.css';
+
+//Check for token
+if (localStorage.jwtToken) {
+  //set auth token to the header
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info and exp
+  const decode = jwt_decode(localStorage.jwtToken);
+  //Set the user and is authenticated
+  store.dispatch(setCurrentUser(decode));
+}
 
 class App extends Component {
   render() {
