9ff6659ec80ffc54fd99107e536ce2f032a0eebf
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-17 18:58:31 +0100

    remove unused class

--- src/components/auth/Regsiter.js
+++ src/components/auth/Regsiter.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import { connect } from 'react-redux';
 
 import { registerUser } from '../../actions/authActions';
