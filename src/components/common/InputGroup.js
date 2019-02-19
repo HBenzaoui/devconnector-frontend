0d686e395bbfb57b13b0c8a42907871f71cdeca5
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-19 18:41:25 +0100

    finishing Text fileds

--- src/components/common/InputGroup.js
+++ src/components/common/InputGroup.js
@@ -2,17 +2,23 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextAreaFieldGroup = ({
+const InputGroup = ({
   name,
   placeholder,
   value,
   error,
-  info,
+  icon,
+  type,
   onChange
 }) => {
   return (
-    <div className="form-group">
-      <textarea
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon} />
+        </span>
+      </div>
+      <input
         className={classnames('form-control form-control-lg', {
           'is-invalid': error
         })}
@@ -21,19 +27,23 @@ const TextAreaFieldGroup = ({
         value={value}
         onChange={onChange}
       />
-      {info && <small className="form-text text-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
 
-TextAreaFieldGroup.propTypes = {
+InputGroup.propTypes = {
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   error: PropTypes.string,
-  info: PropTypes.string,
+  icon: PropTypes.string,
+  type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired
 };
 
-export default TextAreaFieldGroup;
+InputGroup.deffaultProps = {
+  type: 'text'
+};
+
+export default InputGroup;
