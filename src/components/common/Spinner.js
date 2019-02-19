9fa786f08a95d62593abc0dcc64fc59dc9886085
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-19 16:41:51 +0100

    add spinner to dashboard

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -4,7 +4,11 @@ import spinner from '../../img/spinner.gif';
 const Spinner = () => {
   return (
     <div>
-      <img src={spinner} alt="Loading..." />
+      <img
+        src={spinner}
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+        alt="Loading..."
+      />
     </div>
   );
 };
