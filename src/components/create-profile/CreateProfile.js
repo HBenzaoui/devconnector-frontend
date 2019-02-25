06922393899e920e47b963d6c4b0e27b20ad5a0c
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-25 17:28:07 +0100

    add show social network links button

--- src/components/create-profile/CreateProfile.js
+++ src/components/create-profile/CreateProfile.js
@@ -126,6 +126,19 @@ export class CreateProfile extends Component {
                   error={errors.bio}
                   info="Tell us about you"
                 />
+                <div className="mb-3">
+                  <button
+                    onClick={() => {
+                      this.setState(prevState => ({
+                        displaySocialInputs: !prevState.displaySocialInputs
+                      }));
+                    }}
+                    className="btn btn-light"
+                  >
+                    Add Social Network Links
+                  </button>
+                  <span className="text-muted"> Optional</span>
+                </div>
               </form>
             </div>
           </div>
