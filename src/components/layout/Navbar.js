8caebc5e0ce916b7bad5834da13de9b062a7abbd
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-26 20:38:51 +0100

    add dahboard component to navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -18,6 +18,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link to="/dashboard" className="nav-link">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a href="" className="nav-link" onClick={this.handleLogout}>
             <img
               className="rounded-circle"
