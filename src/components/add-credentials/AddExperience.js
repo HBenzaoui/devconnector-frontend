ef511fa17d48d20ad583915f43da0a871fbdc063
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-28 20:43:45 +0100

    start working on edding experinace

--- src/components/add-credentials/AddExperience.js
+++ src/components/add-credentials/AddExperience.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { Link, withRruter } from 'react-router-dom';
+import { TextFieldGroup } from '../common/TextFieldGroup';
+import { TextAreaFieldGroup } from '../common/TextAreaFieldGroup';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export class AddExperience extends Component {
+class AddExperience extends Component {
+  state = {
+    company: '',
+    title: '',
+    location: '',
+    from: '',
+    to: '',
+    current: false,
+    description: '',
+    errors: {},
+    disabled: false
+  };
+
   render() {
     return (
       <div>
