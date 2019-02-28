b8bd59dea2fc1827d2813a9f5708e7cf96e3227b
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-28 20:16:44 +0100

    start working on adding education and experiences

---
+++ src/components/add-credentials/AddEducation.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class AddEducation extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Educations</h1>
+      </div>
+    )
+  }
+}
+
+export default AddEducation
---
+++ src/components/add-credentials/AddExperience.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class AddExperience extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Experience</h1>
+      </div>
+    );
+  }
+}
+
+export default AddExperience;
